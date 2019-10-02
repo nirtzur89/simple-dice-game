f2bbed619a9a3a8604a3edc35b28f792ecbf3b82
nirtzur <nirtzur89@gmail.com>
2019-10-02 15:02:24 +0200

    done

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ var randomNumber2 = random()
   document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
   document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "player 1 won"
+    document.querySelector("h1").textContent = " ⭐️ player 1 won"
+    document.querySelectorAll(".dice")[0].classList.toggle('winner')
   } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "player 2 won"
+    document.querySelector("h1").textContent = "player 2 won ⭐️"
+    document.querySelectorAll(".dice")[1].classList.toggle('winner')
   } else {
     document.querySelector("h1").textContent = "It's a draw"
   }
